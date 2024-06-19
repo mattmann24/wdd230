f7dcd91af52d24446f2e99cfd2f7487cbd41c46d
mattmann24 <mdshank24@gmail.com>
2024-06-19 13:11:25 -0600

    complete week8 home page

--- scripts/password.js
+++ scripts/password.js
@@ -4,7 +4,6 @@ const message = document.querySelector("#formmessage");
 
 kp2.addEventListener("focusout", checkSame);
 
-// This should be refactored.
 function checkSame() {
 	if (kp1.value !== kp2.value) {
 		message.textContent = "❗Passwords DO NOT MATCH!";
@@ -30,22 +29,3 @@ function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-	// Function to handle form submission
-	function handleSubmit(event) {
-		// Prevent default form submission
-		event.preventDefault();
-
-		// Access form elements
-		let formt = event.target;
-		let formData = new FormData(formt);
-
-		// Display form element values
-		for (let pair of formData.entries()) {
-			console.log(pair[0] + ": " + pair[1]);
-		}
-	}
-
-	const form = document.querySelector("form");
-	form.addEventListener("submit", handleSubmit);
-});
