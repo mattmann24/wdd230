4667b4631fea3acd1eb835110a52c7154e77bbaf
mattmann24 <mdshank24@gmail.com>
2024-06-19 11:33:42 -0600

    Record page complete

---
+++ scripts/password.js
@@ -0,0 +1,51 @@
+const kp1 = document.querySelector("#password");
+const kp2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗Passwords DO NOT MATCH!";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+}
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	// Function to handle form submission
+	function handleSubmit(event) {
+		// Prevent default form submission
+		event.preventDefault();
+
+		// Access form elements
+		let formt = event.target;
+		let formData = new FormData(formt);
+
+		// Display form element values
+		for (let pair of formData.entries()) {
+			console.log(pair[0] + ": " + pair[1]);
+		}
+	}
+
+	const form = document.querySelector("form");
+	form.addEventListener("submit", handleSubmit);
+});
