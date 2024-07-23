94a534d3db6eda89cbdab3dd35be8c02f28ddf5f
mattmann24 <mdshank24@gmail.com>
2024-07-23 12:02:42 -0600

    Commiting corrections

--- scooter/scripts/weather.js
+++ scooter/scripts/weather.js
@@ -2,12 +2,16 @@ const currentTemp = document.querySelector('#currentTemp');
 const currentHumid = document.querySelector('#currentHumid');
 const weatherIcon = document.querySelector('#weatherIcon');
 const captionDesc = document.querySelector('figcaption');
-const weatherTitle = document.querySelector('weatherTitle');
+const weatherTitle = document.querySelector('#weatherTitle');
+const mainTitle = document.querySelector('#mainTitle');
+const maxTemp = document.querySelector('#maxTemp');
 
 const tomorrowTemp = document.querySelector('#tomorrowTemp');
 const tomorrowHumid = document.querySelector('#tomorrowHumid');
 const tomorrowIcon = document.querySelector('#tomorrowIcon');
-const tomorrowTitle = document.querySelector('tomorrowTitle');
+const tomorrowTitle = document.querySelector('#tomorrowTitle');
+const tomorrowMainTitle = document.querySelector('#tomorrowMainTitle');
+
 
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=20.44165743237060&lon=-86.9195863139801&units=imperial&appid=54df2e2c323acaf99ee1cee513fc00bc'
@@ -45,43 +49,60 @@ async function apiFetchForcast() {
 }
 
 async function displayResult(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const currentTempdata = data.main.temp;
     const title = data.weather[0].main;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     const humidity = data.main.humidity;
+    const maxTempdata = data.main.temp_max;
 
-    currentHumid.textContent = `${humidity}%`;
+    maxTempRound = Math.round(maxTempdata);
+
+    weatherRound = Math.round(currentTempdata);
+    
+
+    currentHumid.textContent = `Humidity ${humidity}%`;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     weatherTitle.textContent = `${desc}`;
+    currentTemp.innerHTML = `Temperature ${weatherRound} &deg;F `;
     mainTitle.textContent = `${title}`
+    maxTemp.innerHTML = `${maxTempRound} &deg;F `;
 
 
 }
 apiFetch();
+
 apiFetchForcast();
 
 
 async function tomorrowForcast(forcastData){
-    forcastround = Math.round(forcastData.list[8].main.temp);
-    forcastTemp.innerHTML = `${forcastround}&deg;F`;
 
-    const iconsrc1 = `https://openweathermap.org/img/w/${forcastData.list[1].weather[0].icon}.png`;
-    let forcastDesc1 = forcastData.list[8].weather[0].description;
+    forcastTemp = forcastData.list[7].main.temp;
+
+    forcastRound = Math.round(forcastTemp);
+  
+   
+    forcastHumid = forcastData.list[7].main.humidity;
+
+    const iconsrc1 = `https://openweathermap.org/img/w/${forcastData.list[7].weather[0].icon}.png`;
+    let forcastDesc1 = forcastData.list[7].weather[0].description;
     tomorrowIcon.setAttribute('src', iconsrc1);
     tomorrowIcon.setAttribute('alt', forcastDesc1);
+    tomorrowMainTitleData = forcastData.list[7].weather[0].main;
 
     tomorrowTitle.textContent = `${forcastDesc1}`;
-    
-    const dateStr1 = forcastData.list[8].dt_txt;
+    tomorrowTemp.innerHTML = `Temperature: ${forcastRound} &deg;F `;
+    tomorrowHumid.textContent = `Humidity: ${forcastHumid}%`;
+    tomorrowMainTitle.textContent = `${tomorrowMainTitleData}`;
+    const dateStr1 = forcastData.list[7].dt_txt;
     const day1 = getDayName(dateStr1, "en-US");
     forcastDay1.innerHTML = day1;
     
     }
 
 
-    
+
     function getDayName(dateStr, locale) {
         var date = new Date(dateStr);
         return date.toLocaleDateString(locale, { weekday: 'short' });        
